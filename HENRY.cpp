d6a14466d59221b2e3058d3015c53ea06f67dcf5
Gabe Rundlett <gabe@rundlett.com>
2018-09-21 14:21:22 -0400

    Commented the source file "HENRY.cpp"

--- HENRY.cpp
+++ HENRY.cpp
@@ -6,10 +6,18 @@ namespace HENRY
 {
 	int Ascii_ISO_3D(const char * data, const unsigned short width)
 	{
-		// bullet proofing
+		/*---------------------
+		|   bullet proofing   |
+		---------------------*/
+		/* run through the char set until it 
+		finds an end character. */
 		unsigned int dataLen = 0;
 		while (data[dataLen] != '\0')
 			dataLen++;
+		/* if the number of characters in the 
+		input data is indivisible by the input
+		width, return a failure code, ending the
+		execution of the function. */
 		if (dataLen % width != 0)
 		{
 			Serial.println("ERROR::ASCII_ISO_3D: Data does not match width!");
@@ -17,13 +25,22 @@ namespace HENRY
 			Serial.println("                   char set does not have a constant width");
 			return 0;
 		}
-		// start the drawing process
+		/*---------------------
+		|   drawing process   |
+		---------------------*/
+		/* calculate the height of the "image" by 
+		dividing the number of characters in the 
+		image by the input width. */
 		const unsigned short height = dataLen / width;
+		/* naive way of having an adjustable value. */
 		const unsigned short scale = 1;
+		/* run through all of the input characters. */
 		for (unsigned short y = 0; y < height * scale; y++)
 		{
 			for (unsigned short x = 0; x < width * scale; x++)
 			{
+				/* test all the values to change the representation 
+				of the image to be isometric 3D. */
 				unsigned short currentIndex = x / scale + y / scale * width;
 				if (data[currentIndex] == '#')
 					Serial.print("\\\\");
@@ -59,7 +76,11 @@ namespace HENRY
 					}
 				}
 			}
+			/* Start a new line after all the characters 
+			have been printed in the line. */
 			Serial.println(' ');
+			/* offset all the lines at the beginning 
+			to make it look slanted. */
 			for (unsigned short i = 0; i < y + 1; i++)
 				Serial.print(' ');
 		}
