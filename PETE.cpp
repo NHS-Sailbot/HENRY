0ce768156403b6e7323424fca4898745ba88b763
Gabe Rundlett <gabe@rundlett.com>
2018-11-28 13:55:11 -0500

    Added ability to assign motor pins with macros

--- PETE.cpp
+++ PETE.cpp
@@ -3,18 +3,17 @@
 namespace PETE
 {
 
-	/* Motor is a class that, with a pair of pins,
-	controls a Linear Actuator with a motor controller
-	using pulse width modulation. when used in code, 
-	just instantiate the object and call the function
-	'.drive(a, b)' to control the motor. parameter 'a'
-	controls how fast the motor runs and can be anything
-	from 0 - 255. parameter b controls the direction and 
-	can either be HIGH or LOW.
-		
+	/* Motor is a class that, with a pair of pins, controls a Linear
+	Actuator with a motor controller using pulse width modulation.
+	when used in code, just instantiate the object and call the
+	function '.drive(a, b)' to control the motor. parameter 'a'
+	controls how fast the motor runs and can be anything from 0 - 255.
+	parameter b controls the direction and can either be HIGH or LOW.
+
 		for example:
 	------------------------------
 		PETE::Motor myPeteMotor(3, 2); // set PWM pin to 3 and direction pin to 2
+		or: - - - - myPeteMotor(MOTOR_A)
 		...
 		myPeteMotor.drive(255, HIGH); // set to push at max speed
 		delay(1000) // push out for one second
@@ -28,17 +27,25 @@ namespace PETE
 	how fast or slow the motor travels, whereas the digital direction
 	pin sends either HIGH or LOW dictating whether to go in or out. */
 	Motor::Motor(unsigned char pin_pwm, unsigned char pin_dir)
-		m_pinPWM()
+		: m_pinPWM(pin_pwm), m_pinDirection(pin_dir)
 	{
-		pinMode(pin_pwm, OUTPUT);
-		pinMode(pin_dir, OUTPUT);
+		pinMode(m_pinPWM, OUTPUT);
+		pinMode(m_pinDirection, OUTPUT);
+	}
+
+	Motor::Motor(bool motor_number)
+		: m_pinPWM(motor_number ? 3 : 9), m_pinDirection(motor_number ? 2 : 8) 
+	{
+		pinMode(m_pinPWM, OUTPUT);
+		pinMode(m_pinDirection, OUTPUT);
 	}
 
 	/* This function just sets the values for which the motor runs with.
-	By this I mean once this is called, the motor will run at the specified
-	speed and direction until it is told to do otherwise, thus if you call
-	this function once and only once with values telling it to go out as 
-	fast as possible, it will go *out* ... as fast as possible forever.*/
+	By this I mean once this is called, the motor will run at the
+	specified speed and direction until it is told to do otherwise, thus
+	if you call this function once and only once with values telling it
+	to go out as fast as possible, it will go out ... as fast as
+	possible, forever.*/
 	void Motor::drive(unsigned char power, unsigned char direction)
 	{
 		digitalWrite(m_pinDirection, direction);
