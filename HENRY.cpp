6c2799a7abfb4db0334de40fc3b9141d2454ef4b
Gabe Rundlett <gabe@rundlett.com>
2019-04-08 19:56:50 -0400

    new code being catered towards the new motors. temporary, old code will be reinstated

--- HENRY.cpp
+++ HENRY.cpp
@@ -1,102 +1,31 @@
 #include "HENRY.h"
+#include "ext-Encoder.h"
 
 namespace HENRY
 {
-	/* A function that takes in a set of data
-	that represents an image and it's width,
-	and it prints it to the serial monitor
-	in isometric 3D.
-
-		for example:
-	------------------------------
-		const char * image =
-		" # # # #"
-		"# # # # "
-		"  ##  ##"
-		"##  ##  ";
-
-		Ascii_ISO_3D(image, 8);
-	------------------------------*/
-	bool Ascii_ISO_3D(const char * data, const unsigned short width)
+	
+	Motor::Motor(unsigned short pin_a, unsigned short pin_b)
+		: m_pinA(pin_a), m_pinB(pin_b)
 	{
-		/*---------------------
-		|   bullet proofing   |
-		---------------------*/
-		/* run through the char set until it 
-		finds an end character. */
-		unsigned int dataLen = 0;
-		while (data[dataLen] != '\0')
-			dataLen++;
-		/* if the number of characters in the 
-		input data is indivisible by the input
-		width, return a failure code, ending the
-		execution of the function. */
-		if (dataLen % width != 0)
-		{
-			Serial.println("ERROR::ASCII_ISO_3D: Data does not match width!");
-			Serial.println("  Possible causes: input width is not the width of the image");
-			Serial.println("                   char set does not have a constant width");
-			return 1;
-		}
-		/*---------------------
-		|   drawing process   |
-		---------------------*/
-		/* calculate the height of the "image" by 
-		dividing the number of characters in the 
-		image by the input width. */
-		const unsigned short height = dataLen / width;
-		/* naive way of having an adjustable value. */
-		const unsigned short scale = 1;
-		/* run through all of the input characters. */
-		for (unsigned short y = 0; y < height * scale; y++)
-		{
-			for (unsigned short x = 0; x < width * scale; x++)
-			{
-				/* test all the values to change the representation 
-				of the image to be isometric 3D. */
-				unsigned short currentIndex = x / scale + y / scale * width;
-				if (data[currentIndex] == '#')
-					Serial.print("\\\\");
-				else
-				{
-					if (data[currentIndex + 1] == ' ')
-					{
-						if (data[currentIndex - width] == '#')
-						{
-							if (data[currentIndex - width + 1] == ' ')
-								Serial.print("/ ");
-							else
-								Serial.print("//");
-						}
-						else if (data[currentIndex - width + 1] == '#')
-							Serial.print("\\/");
-						else
-							Serial.print("  ");
-					}
-					else if (data[currentIndex - width + 1] == '#')
-					{
-						if (data[currentIndex - width] == '#')
-							Serial.print("//");
-						else
-							Serial.print("\\/");
-					}
-					else
-					{
-						if (data[currentIndex - width] == '#')
-							Serial.print("//");
-						else
-							Serial.print(" /");
-					}
-				}
-			}
-			/* Start a new line after all the characters 
-			have been printed in the line. */
-			Serial.println(' ');
-			/* offset all the lines at the beginning 
-			to make it look slanted. */
-			for (unsigned short i = 0; i < y + 1; i++)
-				Serial.print(' ');
-		}
-		return 0;
+		init();
+	}
+
+	void Motor::init()
+	{
+		pinMode(m_pinA, OUTPUT);
+		pinMode(m_pinB, OUTPUT);
+	}
+
+	void Motor::init(unsigned short pin_a, unsigned short pin_b)
+	{
+		m_pinA = pin_a;
+		m_pinB = pin_b;
+		pinMode(pin_a, OUTPUT);
+		pinMode(pin_b, OUTPUT);
+	}
+
+	inline void Motor::update()
+	{
+
 	}
 }
--- HENRY.h
+++ HENRY.h
@@ -1,10 +1,31 @@
 #pragma once
 #include <Arduino.h>
-#include "Boat.h"
 
 namespace HENRY
 {
 
-	bool Ascii_ISO_3D(const char * data, const unsigned short width);
+	// pin a is power (through "PWM" pulse width modulation) - analog
+	// pin b is direction - digital, either HIGH or LOW
+
+	class Motor
+	{
+		long m_prevPos, m_currPos;
+		unsigned short m_pinA, m_pinB;
+	public:
+		Motor() {}
+		Motor(unsigned short pin_a, unsigned short pin_b);
+
+		void init();
+		void init(unsigned short pin_a, unsigned short pin_b);
+
+		inline void update();
+
+		inline unsigned short getPos() { return m_currPos; }
+		
+		inline void setPow(unsigned short pow)
+			{ analogWrite(m_pinA, pow); }
+		inline void setDir(unsigned short dir)
+			{ digitalWrite(m_pinB, dir); }
+	};
 
 }
