14718371dd52f899aa63e4c9140d30ce077eb45b
GabeRundlett <gabe@rundlett.com>
2019-04-12 18:04:44 -0400

    update HENRY

--- src/HENRY.h
+++ src/HENRY.h
@@ -5,3 +5,9 @@
 #include "Magnetometer.h"
 #include "Motor.h"
 #include "Boat.h"
+
+namespace HENRY
+{
+bool Ascii_ISO_3D(const char *message);
+bool Ascii_ISO_3D(const char *data, const unsigned short width);
+}
