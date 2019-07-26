37e6dda792576baff0f2fa16f5cc60d803bcde27
Gabe Rundlett <gabe@rundlett.com>
2019-07-26 17:46:51 -0400

    Added a resettable timer in 'sailbot::clock`

--- sailbot/src/sailbot/timing.cpp
+++ sailbot/src/sailbot/timing.cpp
@@ -7,3 +7,13 @@ namespace sailbot { namespace timing {
         std::this_thread::sleep_for(std::chrono::milliseconds(static_cast<long long>(seconds * 1000)));
     }
 }} // namespace sailbot::timing
+
+namespace sailbot { namespace clock {
+    std::chrono::time_point<std::chrono::steady_clock> start;
+    void reset() { start = std::chrono::steady_clock::now(); }
+    double now() {
+        return static_cast<double>(
+                   std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start).count()) /
+               1000000;
+    }
+}} // namespace sailbot::clock
--- sailbot/src/sailbot/timing.hpp
+++ sailbot/src/sailbot/timing.hpp
@@ -3,3 +3,8 @@
 namespace sailbot { namespace timing {
     void sleep(double seconds);
 }} // namespace sailbot::timing
+
+namespace sailbot { namespace clock {
+    void reset();
+    double now();
+}} // namespace sailbot::clock
