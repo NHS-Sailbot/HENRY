e3ecdc4a141ecf118109cb1d9b588ab8556bdea6
Gabe Rundlett <gabe@rundlett.com>
2019-07-29 08:55:46 -0400

    updated system handshake API

--- README.md
+++ README.md
@@ -31,27 +31,23 @@ struct RData {
     char bytes[8];
 };
 
-void print_data(const RData &d) {
-    std::cout << d.bytes << ' ';
-    for (unsigned int i = 0; i < 3; ++i)
-        std::cout << d.words[i] << ' ';
-    std::cout << '\n';
-}
-
 TData tdata = {"Computer", {0.4, 0.5, 0.6}};
 RData rdata;
 
 int main() {
-    sailbot::system::init();
+    if (sailbot::system::init())
+        return 1;
 
-    while (sailbot::system::update()) {
-        if (sailbot::system::should_tick()) {
-            sailbot::system::handshake(&tdata, sizeof(TData), &rdata, sizeof(RData));
-            print_data(rdata);
-        }
+    while (sailbot::system::update(&tdata, sizeof(TData), &rdata, sizeof(RData))) {
+        std::cout << rdata.bytes << ' ';
+        for (unsigned int i = 0; i < 3; ++i)
+            std::cout << rdata.words[i] << ' ';
+        std::cout << '\n';
     }
 }
+
 ```
+
 ### [.../test.ino]
 ```cpp
 struct RData {
--- sailbot/src/sailbot/system.cpp
+++ sailbot/src/sailbot/system.cpp
@@ -1,37 +1,35 @@
+#include "system.hpp"
 #include "comm.hpp"
 #include "timing.hpp"
 #include <iostream>
 
 namespace sailbot { namespace system {
 #ifndef _CONFIG_PLATFORM_WINDOWS
-    constexpr unsigned int PORT = 19, BAUD_RATE = 57600;
+    constexpr unsigned int PORT = 19;
 #else
-    constexpr unsigned int PORT = 2, BAUD_RATE = 57600;
+    constexpr unsigned int PORT = 2;
 #endif
+    constexpr unsigned int BAUD_RATE = 57600;
     int init() {
         const int result = sailbot::comm::open_device_err(PORT, BAUD_RATE);
         clock::reset();
         return result;
     }
-    bool update() { return true; }
-    constexpr static double TICK_DURATION = 1.0 / 20;
+    constexpr static double TICK_DURATION = 1.0 / 100;
     static double s_current_time = 0, s_total_tick_time = 0;
     static unsigned long long s_total_tick_count = 0;
     static bool s_transmission_switch = true;
-    bool should_tick() {
+    bool update(const void *tdata, const unsigned int tsize, void *rdata, const unsigned int rsize) {
         s_current_time = clock::now();
         if (s_current_time > s_total_tick_time + TICK_DURATION) {
             s_total_tick_count++;
             s_total_tick_time = s_current_time;
             s_transmission_switch = s_total_tick_count % 2;
-            return true;
-        } else
-            return false;
-    }
-    void handshake(const void *tdata, const unsigned int tsize, void *rdata, const unsigned int rsize) {
-        if (s_transmission_switch)
-            sailbot::comm::write_buffer(PORT, tdata, tsize);
-        else
-            sailbot::comm::read_buffer(PORT, rdata, rsize);
+            if (s_transmission_switch)
+                sailbot::comm::write_buffer(PORT, tdata, tsize);
+            else
+                sailbot::comm::read_buffer(PORT, rdata, rsize);
+        }
+        return true;
     }
 }} // namespace sailbot::system
