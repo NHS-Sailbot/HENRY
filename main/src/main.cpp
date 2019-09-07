681a8376786e5782d3ef5ff8a2083465adb2ded8
GabeRundlett <gabe@rundlett.com>
2019-09-07 12:27:39 -0400

    Working commit of the Camera API

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,8 +1,6 @@
 #include <sailbot.hpp>
 #include <stdio.h>
 
-#include "shaders.hpp"
-
 struct TData {                     // size is 6 bytes
     unsigned char m1_dir, m2_dir;  // 1 byte (uchar)
     unsigned short m1_pow, m2_pow; // 2 bytes (ushort)
@@ -29,42 +27,81 @@ void tick() {
     frames_per_tick = 0;
 }
 
+static constexpr char *vert_src = R"(
+    #version 330 core
+    layout(location = 0) in vec2 pos;
+    layout(location = 1) in vec2 tex;
+    layout(location = 2) in vec4 col;
+    layout(location = 3) in float tid;
+    
+    out vec2 v_tex;
+    
+    void main() {
+        v_tex = tex;
+        gl_Position = vec4(pos, 0, 1);
+    }
+)";
+
+static constexpr char *frag_src = R"(
+    #version 330 core
+    in vec2 v_tex;
+    
+    out vec4 color;
+    
+    uniform sampler2D tex;
+    
+    vec4 yuv422_rgb(in vec4 col) {
+        vec4 result;
+        int c = int(col.x * 256 - 16);
+        int d = int(col.y * 256 - 128);
+        int e = int(col.z * 256 - 128);
+        return vec4(
+            clamp(float((298 * c + 409 * e + 128) >> 8) / 256, 0, 256),
+            clamp(float((298 * c - 100 * d - 208 * e + 128) >> 8) / 256, 0, 256),
+            clamp(float((298 * c + 516 * d + 128) >> 8) / 256, 0, 256),
+            1.0
+        );
+    }
+    
+    void main() {
+        color = vec4(v_tex, 0, 1);
+        color = yuv422_rgb(texture(tex, v_tex)); // texture(tex, v_tex);
+    }
+)";
+
 int main() {
+    sailbot::system::init("/dev/ttyACM0", 9600);
     sailbot::camera::open();
 
-    sailbot::callbacks::set::on_data_read(tick);
-    sailbot::system::init("/dev/ttyACM0", 115200);
-
-    coel::Window window(sailbot::camera::width, sailbot::camera::height, "window");
+    coel::Window window(sailbot::camera::width, sailbot::camera::height, "test");
     coel::Texture texture(sailbot::camera::width, sailbot::camera::height, coel::ColorSpace::RGB);
-    coel::Shader shader(shaders::vert_src, shaders::frag_src);
-
+    coel::Shader shader(vert_src, frag_src);
     coel::renderer::batch2d::init();
+
+    double last_time = window.get_time();
+    unsigned char image_data[sailbot::camera::width * sailbot::camera::height * 3];
 
     while (!window.should_close()) {
         sailbot::system::update(&tdata, sizeof(tdata), &rdata, sizeof(rdata));
 
-        unsigned char image_data[sailbot::camera::width * sailbot::camera::height * 3];
         unsigned char *buffer_start = sailbot::camera::read();
         if (buffer_start == nullptr) buffer_start = image_data;
         unsigned char *cam_y = buffer_start, *cam_u = buffer_start + 1, *cam_v = buffer_start + 3;
-        for (unsigned int y = 0; y < sailbot::camera::height; ++y) {
-            for (unsigned int x = 0; x < sailbot::camera::width; ++x) {
-                const unsigned int index = x * 3 + y * sailbot::camera::width * 3;
-                image_data[index + 0] = *cam_y;
-                image_data[index + 1] = *cam_u;
-                image_data[index + 2] = *cam_v;
-                if (index % 2) { cam_u += 4, cam_v += 4; }
-                cam_y += 2;
-            }
+        for (unsigned int i = 0; i < sailbot::camera::width * sailbot::camera::height * 3; i += 3) {
+            image_data[i + 0] = *cam_y;
+            image_data[i + 1] = *cam_u;
+            image_data[i + 2] = *cam_v;
+            if (i % 2) { cam_u += 4, cam_v += 4; }
+            cam_y += 2;
         }
+
         texture.update(image_data);
+
         coel::renderer::clear();
         coel::renderer::batch2d::submit_rect(-1, 1, 2, -2);
         coel::renderer::batch2d::flush();
 
         window.update();
-        frames_per_tick++;
     }
 
     sailbot::camera::close();
--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,31 +1,5 @@
 #include <sailbot.hpp>
 
-#include <iostream>
-#include <memory.h>
-#include <sys/file.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
-#include <unistd.h>
-
-#include <asm/types.h>
-#include <linux/videodev2.h>
-
-#include <linux/v4l2-subdev.h>
-
-static constexpr unsigned int capture_width = 1280, capture_height = 720;
-struct Vertex {
-    float x, y, u, v;
-};
-
-static constexpr Vertex vertices[]{
-    {-1, -1, 0, 1},
-    {-1, 1, 0, 0},
-    {1, -1, 1, 1},
-    {1, 1, 1, 0},
-};
-
-static constexpr unsigned short indices[] = {0, 1, 2, 1, 3, 2};
-
 static constexpr char *vert_src = R"(
     #version 330 core
     layout(location = 0) in vec2 pos;
@@ -50,85 +24,54 @@ static constexpr char *frag_src = R"(
     uniform sampler2D tex;
     
     vec4 yuv422_rgb(in vec4 col) {
-        col -= vec4(0.0625, 0.5, 0.5, 0);
-        vec4 result = vec4(
-            1.1640625 * col.x + 1.59765625 * col.z + 0.5,
-            0,0,1
+        vec4 result;
+        int c = int(col.x * 256 - 16);
+        int d = int(col.y * 256 - 128);
+        int e = int(col.z * 256 - 128);
+        return vec4(
+            clamp(float((298 * c + 409 * e + 128) >> 8) / 256, 0, 256),
+            clamp(float((298 * c - 100 * d - 208 * e + 128) >> 8) / 256, 0, 256),
+            clamp(float((298 * c + 516 * d + 128) >> 8) / 256, 0, 256),
+            1.0
         );
-        return result;
     }
     
     void main() {
         color = vec4(v_tex, 0, 1);
-        color = yuv422_rgb(texture(tex, v_tex));
+        color = yuv422_rgb(texture(tex, v_tex)); // texture(tex, v_tex);
     }
 )";
-
-// const int c = *cam_y - 16, d = *cam_u - 128, e = *cam_v - 128;
-// image_r = clamp((298 * c + 409 * e + 128) >> 8);
-// image_g = clamp((298 * c - 100 * d - 208 * e + 128) >> 8);
-// image_b = clamp((298 * c + 516 * d + 128) >> 8);
 
 int main() {
     sailbot::camera::open();
 
-    coel::Window window(capture_width, capture_height, "test");
-    coel::renderer::batch2d::init();
+    coel::Window window(sailbot::camera::width, sailbot::camera::height, "test");
+    coel::Texture texture(sailbot::camera::width, sailbot::camera::height, coel::ColorSpace::RGB);
     coel::Shader shader(vert_src, frag_src);
-    coel::Texture texture(capture_width, capture_height, coel::ColorSpace::RGB);
+    coel::renderer::batch2d::init();
 
     double last_time = window.get_time();
+    unsigned char image_data[sailbot::camera::width * sailbot::camera::height * 3];
+
     while (!window.should_close()) {
-        const double time = window.get_time();
-        const double elapsed = time - last_time;
-        last_time = time;
-        std::cout << "elapsed time is " << elapsed << '\n';
-        unsigned char image_data[capture_width * capture_height * 3];
         unsigned char *buffer_start = sailbot::camera::read();
-        if (buffer_start == nullptr) {
-            printf(".");
-            buffer_start = image_data;
-        }
+        if (buffer_start == nullptr) buffer_start = image_data;
         unsigned char *cam_y = buffer_start, *cam_u = buffer_start + 1, *cam_v = buffer_start + 3;
-        for (unsigned int y = 0; y < capture_height; ++y) {
-            for (unsigned int x = 0; x < capture_width; ++x) {
-                const unsigned int index = x * 3 + y * capture_width * 3;
-                image_data[index] = *cam_y;
-                image_data[index + 1] = *cam_u;
-                image_data[index + 2] = *cam_v;
-                if (index % 2) { cam_u += 4, cam_v += 4; }
-                cam_y += 2;
-                // const unsigned int index = x * 3 + y * capture_width * 3;
-                // unsigned char &image_r = image_data[index];
-                // unsigned char &image_g = image_data[index + 1];
-                // unsigned char &image_b = image_data[index + 2];
-                // const int c = *cam_y - 16, d = *cam_u - 128, e = *cam_v - 128;
-                // image_r = clamp((298 * c + 409 * e + 128) >> 8);
-                // image_g = clamp((298 * c - 100 * d - 208 * e + 128) >> 8);
-                // image_b = clamp((298 * c + 516 * d + 128) >> 8);
-                // if (index % 2) { cam_u += 4, cam_v += 4; }
-                // cam_y += 2;
-            }
+        for (unsigned int i = 0; i < sailbot::camera::width * sailbot::camera::height * 3; i += 3) {
+            image_data[i + 0] = *cam_y;
+            image_data[i + 1] = *cam_u;
+            image_data[i + 2] = *cam_v;
+            if (i % 2) { cam_u += 4, cam_v += 4; }
+            cam_y += 2;
         }
-
-        //
-        //
-        //
-        //
-        //
-
         texture.update(image_data);
 
         coel::renderer::clear();
-        coel::renderer::batch2d::submit_rect(-0.9, 0.9, 1.8, -1.8);
+        coel::renderer::batch2d::submit_rect(-1, 1, 2, -2);
         coel::renderer::batch2d::flush();
 
         window.update();
     }
 
-    //
-    //
-    //
-    //
-    //
+    sailbot::camera::close();
 }
