ff649f96285b4b3609064ae183ca6697a1ac5e27
GabeRundlett <gabe@rundlett.com>
2020-01-23 07:18:17 -0500

    Update

--- Master/Master.cpp
+++ Master/Master.cpp
@@ -3,81 +3,75 @@
 
 #include <iostream>
 
-static inline constexpr double rcRange(const unsigned short aVal, const unsigned short aMinVal, const unsigned short aMidVal, const unsigned short aMaxVal) {
-	if (aVal > aMidVal) {
-		if (aVal > aMaxVal) return 0;
-		return static_cast<double>(aVal - aMidVal) / (aMidVal - aMinVal);
-	} else {
-		if (aVal < aMinVal) return 0;
-		return static_cast<double>(aVal - aMidVal) / (aMaxVal - aMidVal);
-	}
+static inline constexpr double rcRange(const unsigned short aVal, const unsigned short aMinVal, const unsigned short aMidVal,
+                                       const unsigned short aMaxVal) {
+    if (aVal > aMidVal) {
+        if (aVal > aMaxVal) return 0;
+        return static_cast<double>(aVal - aMidVal) / (aMidVal - aMinVal);
+    } else {
+        if (aVal < aMinVal) return 0;
+        return static_cast<double>(aVal - aMidVal) / (aMaxVal - aMidVal);
+    }
 }
 
 int main() {
-	struct MasterR {
-		unsigned int mWindSensorReading;
-		int mMotorReading;
-	};
-	struct MasterT {
-		int mMotorDesired;
-	};
-	struct RemoteR {
-		unsigned short mRC[6];
-	};
-	struct RemoteT {
-	};
+    struct MasterR {
+        unsigned int mWindSensorReading;
+        int mMotorReading;
+    };
+    struct MasterT {
+        int mMotorDesired;
+    };
+    struct RemoteR {
+        unsigned short mRC[6];
+    };
+    struct RemoteT {};
 
-	Henry::Arduino masterArd(sizeof(MasterR), sizeof(MasterT), 115200, 0xA5);
-	MasterR *masterR = nullptr;
-	MasterT *masterT = nullptr;
-	Henry::Arduino remoteArd(sizeof(RemoteR), sizeof(RemoteT), 115200, 0xBD);
-	RemoteR *remoteR = nullptr;
-	RemoteT *remoteT = nullptr;
+    Henry::Arduino masterArd(sizeof(MasterR), sizeof(MasterT), 115200, 0xA5);
+    MasterR *masterR = nullptr;
+    MasterT *masterT = nullptr;
+    Henry::Arduino remoteArd(sizeof(RemoteR), sizeof(RemoteT), 115200, 0xBD);
+    RemoteR *remoteR = nullptr;
+    RemoteT *remoteT = nullptr;
 
-	Henry::Camera camera(1280, 720);
+    Henry::Camera camera(1280, 720);
 
-	enum StatusFlag {
-		NONE = 0,
-		MASTER_ARD = 1,
-		REMOTE_ARD = 2,
-		CAMERA = 4,
-		ALL = 7
-	};
-	unsigned char statusFlags = ALL;
+    enum StatusFlag { NONE = 0, MASTER_ARD = 1, REMOTE_ARD = 2, CAMERA = 4, ALL = 7 };
+    unsigned char statusFlags = ALL;
 
-	if (!masterArd.isConnected()) {
-		std::cout << "Unable to connect with the motors Arduino\n";
-		statusFlags &= ~MASTER_ARD;
-	} else {
-		masterR = reinterpret_cast<MasterR *>(masterArd.mRdata);
-		masterT = reinterpret_cast<MasterT *>(masterArd.mTdata);
-	}
+    if (!masterArd.isConnected()) {
+        std::cout << "Unable to connect with the motors Arduino\n";
+        statusFlags &= ~MASTER_ARD;
+    } else {
+        masterR = reinterpret_cast<MasterR *>(masterArd.mRdata);
+        masterT = reinterpret_cast<MasterT *>(masterArd.mTdata);
+    }
 
-	if (!remoteArd.isConnected()) {
-		std::cout << "Unable to connect with the remote Arduino\n";
-		statusFlags &= ~REMOTE_ARD;
-	} else {
-		remoteR = reinterpret_cast<RemoteR *>(remoteArd.mRdata);
-		remoteT = reinterpret_cast<RemoteT *>(remoteArd.mTdata);
-	}
+    if (!remoteArd.isConnected()) {
+        std::cout << "Unable to connect with the remote Arduino\n";
+        statusFlags &= ~REMOTE_ARD;
+    } else {
+        remoteR = reinterpret_cast<RemoteR *>(remoteArd.mRdata);
+        remoteT = reinterpret_cast<RemoteT *>(remoteArd.mTdata);
+    }
 
-	if (!camera.isOpen()) {
-		std::cout << "Unable to open the camera\n";
-		statusFlags &= ~CAMERA;
-	}
+    if (!camera.isOpen()) {
+        std::cout << "Unable to open the camera\n";
+        statusFlags &= ~CAMERA;
+    }
 
-	if (statusFlags == ALL) {
-		int motorDesired = 0;
+    if (statusFlags == ALL) {
+        int motorDesired = 0;
 
-		while (true) {
-			if (remoteArd.isValid() && masterArd.isValid()) {
-				auto tJoystick1 = rcRange(remoteR->mRC[0], 991, 1503, 2015);
-				auto tMotorReading = masterR->mMotorReading;
-				if (tJoystick1 > 0.2 || tJoystick1 < -0.2)
-					masterT->mMotorDesired = tMotorReading + tJoystick1 * 100, motorDesired = tMotorReading;
-				else
-					masterT->mMotorDesired = motorDesired;
-			}
-		}
-	}
+        while (true) {
+            if (remoteArd.isValid() && masterArd.isValid()) {
+                auto tJoystick1 = rcRange(remoteR->mRC[0], 991, 1503, 2015);
+                auto tMotorReading = masterR->mMotorReading;
+                if (tJoystick1 > 0.2 || tJoystick1 < -0.2)
+                    masterT->mMotorDesired = tMotorReading + tJoystick1 * 100, motorDesired = tMotorReading;
+                else
+                    masterT->mMotorDesired = motorDesired;
+            }
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Henry
 
 Henry is the public Github Repository created and maintained by the NHS Sailbot team. More information can be found on [the wiki](https://github.com/NHS-Sailbot/Henry/wiki).
+
+## Building from source:
+
+On a default, unmodified Ubuntu installation:
+
+```
+sudo apt install git build-essential cmake libjpeg-dev
+git clone https://github.com/NHS-Sailbot/Henry --recurse-submodules ~/Dev/Henry
+mkdir ~/Dev/Henry/build && cd $_
+cmake -DCMAKE_BUILD_TYPE=Release .. && make -j8
+./Master/Master
+```
